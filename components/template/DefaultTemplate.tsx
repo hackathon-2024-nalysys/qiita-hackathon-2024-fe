3bbc54a4723d70c3af73097f07bf4633d59cba9b
yutaro_matsumoto <spurs03170629@gmail.com>
2024-09-21 15:39:55 +0900

    DefaultTemplateいじった

--- components/template/DefaultTemplate.tsx
+++ components/template/DefaultTemplate.tsx
@@ -1,21 +1,30 @@
 import { FC, ReactNode } from 'react';
-import { AppShell, Flex, rem, useMantineTheme } from '@mantine/core';
+import { AppShell, Container, Flex, rem, Title, useMantineTheme } from '@mantine/core';
 import { Logo } from '@/components/template/Logo/Logo';
 
 type DefaultTemplateProps = {
   children: ReactNode;
+  title: string;
 };
-export const DefaultTemplate: FC<DefaultTemplateProps> = ({ children }) => {
+const HEADER_HEIGHT = 48;
+export const DefaultTemplate: FC<DefaultTemplateProps> = ({ children, title }) => {
   const theme = useMantineTheme();
 
   return (
-    <AppShell header={{ height: { base: 48 } }}>
+    <AppShell header={{ height: { base: HEADER_HEIGHT } }}>
       <AppShell.Header bg={theme.colors.blue[4]}>
         <Flex h="100%" align="center" p={8}>
           <Logo />
         </Flex>
       </AppShell.Header>
-      <AppShell.Main pt={`calc(${rem(48)})`}>{children}</AppShell.Main>
+      <AppShell.Main pt={`calc(${rem(HEADER_HEIGHT)})`}>
+        <Container h={`calc(${rem(`100vh - ${HEADER_HEIGHT}`)})`} pt={16}>
+          <Title order={1} size="h2">
+            {title}
+          </Title>
+          {children}
+        </Container>
+      </AppShell.Main>
     </AppShell>
   );
 };
