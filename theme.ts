d092156edd5c5c0d771e008c476c074f37175774
makkenno <kota.m.2774@gmail.com>
2024-09-21 19:49:04 +0900

    colorを変更

--- theme.ts
+++ theme.ts
@@ -2,5 +2,20 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    yellow: [
+      'FFFBEF',
+      'FFDA5F',
+      'FFC300',
+      '5C4E1F',
+      '5C4E1F',
+      '5C4E1F',
+      '5C4E1F',
+      '5C4E1F',
+      '5C4E1F',
+      '5C4E1F',
+    ],
+  },
   autoContrast: true,
+  primaryColor: 'yellow',
 });
