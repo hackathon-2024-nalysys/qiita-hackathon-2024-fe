d74eaa4675e090b7bc990713c40c66e3f1d88d54
makkenno <kota.m.2774@gmail.com>
2024-09-21 22:58:01 +0900

    headerに変えてみた

--- components/template/DefaultTemplate.tsx
+++ components/template/DefaultTemplate.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react';
 import Link from 'next/link';
 import { IconSearch } from '@tabler/icons-react';
 import { ActionIcon, AppShell, Avatar, Box, Container, Flex, useMantineTheme } from '@mantine/core';
+import { Logo } from './Logo/Logo';
 
 type DefaultTemplateProps = {
   children: ReactNode;
@@ -16,28 +17,36 @@ export const DefaultTemplate: FC<DefaultTemplateProps> = ({ children }) => {
     <AppShell
       style={{ overflow: 'auto' }}
       bg={theme.colors.yellow[0]}
-      footer={{ height: { base: FOOTER_HEIGHT } }}
+      header={{ height: { base: FOOTER_HEIGHT } }}
     >
+      <AppShell.Header>
+        <Flex align="center" justify="space-between" p="md" bg={theme.colors.yellow[2]}>
+          <Logo />
+          <Flex align="center" justify="flex-end" gap="8px">
+            <Link href="/find" style={{ display: 'flex', alignItems: 'center' }}>
+              <ActionIcon size="lg" color={'white'} variant="subtle" aria-label="Find">
+                <IconSearch stroke={2} />
+              </ActionIcon>
+            </Link>
+            <Link href="/mypage" style={{ display: 'flex', alignItems: 'center' }}>
+              <ActionIcon
+                size="lg"
+                style={{ border: '2px solid white' }}
+                radius={'xl'}
+                variant="outline"
+                aria-label="MyPage"
+              >
+                <Avatar src={AVATAR_ICON} size="md" variant="filled" />
+              </ActionIcon>
+            </Link>
+          </Flex>
+        </Flex>
+      </AppShell.Header>
       <AppShell.Main>
-        <Container pt={56} pb={40}>
+        <Container pt={32} pb={40}>
           {children}
         </Container>
       </AppShell.Main>
-      <AppShell.Footer>
-        <Flex h="100%" align="center" p="md" justify="space-between">
-          <Box size="sm" />
-          <Link href="/find">
-            <ActionIcon color={theme.colors.yellow[4]} variant="subtle" aria-label="Find">
-              <IconSearch stroke={1.5} />
-            </ActionIcon>
-          </Link>
-          <Link href="/mypage">
-            <ActionIcon variant="subtle" aria-label="MyPage">
-              <Avatar src={AVATAR_ICON} size="sm" variant="filled" />
-            </ActionIcon>
-          </Link>
-        </Flex>
-      </AppShell.Footer>
     </AppShell>
   );
 };
