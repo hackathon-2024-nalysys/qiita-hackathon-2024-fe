d3cdee0cd7805c01ee761ac80835544db20dec89
yutaro_matsumoto <spurs03170629@gmail.com>
2024-09-21 16:53:26 +0900

    Avatarコンポーネント利用

--- components/pages/find/FindPage.tsx
+++ components/pages/find/FindPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Box, Flex, Image, Title } from '@mantine/core';
+import { Avatar, Box, Flex, Image, Title } from '@mantine/core';
 import { DefaultTemplate } from '@/components/template/DefaultTemplate';
 import { HoverPopOver } from '@/components/ui/HoverPopOver/HoverPopOver';
 
@@ -10,47 +10,49 @@ type TopInterestContentType = {
 };
 const TITLE = 'あなたと同じ趣味を持ってる人リスト';
 
-const TopInterestContent: TopInterestContentType[] = [
-  {
-    tag: { id: '1111', name: 'VTuber' },
-    avatarList: [
-      { id: '1111a', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '1111b', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '1111c', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '1111d', imageSrc: 'https://picsum.photos/76/76' },
-    ],
-  },
-  {
-    tag: { id: '2222', name: 'サッカー' },
-    avatarList: [
-      { id: '2222a', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222b', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222c', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222d', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222e', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222f', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222g', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222h', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '2222i', imageSrc: 'https://picsum.photos/76/76' },
-    ],
-  },
-  {
-    tag: { id: '3333', name: '囲碁' },
-    avatarList: [
-      { id: '3333a', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '3333b', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '3333c', imageSrc: 'https://picsum.photos/76/76' },
-      { id: '3333d', imageSrc: 'https://picsum.photos/76/76' },
-    ],
-  },
-];
-
 // MEMO: ロジックとUIの分岐は余裕があったら！
 export const FindPage: FC = () => {
+  // TODO: APIで取得
   const NearHobyPeople: Avatar[] = [
     { id: '1111aaa', imageSrc: 'https://picsum.photos/76/76' },
     { id: '1111aaa', imageSrc: 'https://picsum.photos/76/76' },
     { id: '1111aaa', imageSrc: 'https://picsum.photos/76/76' },
+  ];
+
+  // TODO: APIで取得
+  const TopInterestContent: TopInterestContentType[] = [
+    {
+      tag: { id: '1111', name: 'VTuber' },
+      avatarList: [
+        { id: '1111a', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '1111b', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '1111c', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '1111d', imageSrc: 'https://picsum.photos/76/76' },
+      ],
+    },
+    {
+      tag: { id: '2222', name: 'サッカー' },
+      avatarList: [
+        { id: '2222a', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222b', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222c', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222d', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222e', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222f', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222g', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222h', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '2222i', imageSrc: 'https://picsum.photos/76/76' },
+      ],
+    },
+    {
+      tag: { id: '3333', name: '囲碁' },
+      avatarList: [
+        { id: '3333a', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '3333b', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '3333c', imageSrc: 'https://picsum.photos/76/76' },
+        { id: '3333d', imageSrc: 'https://picsum.photos/76/76' },
+      ],
+    },
   ];
 
   return (
@@ -77,14 +79,7 @@ export const FindPage: FC = () => {
             <Flex mt={16} gap={16} wrap="wrap">
               {avatarList.map(({ id, imageSrc }) => (
                 <HoverPopOver key={id}>
-                  <Image
-                    style={{ cursor: 'pointer' }}
-                    src={imageSrc}
-                    alt="Avatar"
-                    width="76px"
-                    height="76px"
-                    radius="50%"
-                  />
+                  <Avatar style={{ cursor: 'pointer' }} src={imageSrc} size="xl" radius="50%" />
                 </HoverPopOver>
               ))}
             </Flex>
