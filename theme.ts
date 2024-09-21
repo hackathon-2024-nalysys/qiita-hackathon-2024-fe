886fe9c947cff19a6751de2b541a5d0f998d35e7
makkenno <kota.m.2774@gmail.com>
2024-09-21 20:28:24 +0900

    colorが当たるように変更

--- theme.ts
+++ theme.ts
@@ -2,18 +2,19 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  black: '#5C4E1F',
   colors: {
     yellow: [
-      'FFFBEF',
-      'FFDA5F',
-      'FFC300',
-      '5C4E1F',
-      '5C4E1F',
-      '5C4E1F',
-      '5C4E1F',
-      '5C4E1F',
-      '5C4E1F',
-      '5C4E1F',
+      '#FFFBEF',
+      '#FFDA5F',
+      '#FFC300',
+      '#5C4E1F',
+      '#5C4E1F',
+      '#5C4E1F',
+      '#5C4E1F',
+      '#5C4E1F',
+      '#5C4E1F',
+      '#5C4E1F',
     ],
   },
   autoContrast: true,
