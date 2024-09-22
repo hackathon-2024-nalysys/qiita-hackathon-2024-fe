729e397555514ed79c0566c26ff8de343e940516
makkenno <kota.m.2774@gmail.com>
2024-09-22 10:07:31 +0900

    ログイン画面はヘッダーのアイコンでないように修正

--- components/template/DefaultTemplate.tsx
+++ components/template/DefaultTemplate.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { IconSearch, IconUserSearch } from '@tabler/icons-react';
 import { ActionIcon, AppShell, Avatar, Box, Container, Flex, useMantineTheme } from '@mantine/core';
 import { Logo } from './Logo/Logo';
@@ -12,6 +13,8 @@ const FOOTER_HEIGHT = 60;
 const AVATAR_ICON = '/avatarImg/me.jpg';
 export const DefaultTemplate: FC<DefaultTemplateProps> = ({ children }) => {
   const theme = useMantineTheme();
+  const isLoginPage = useRouter().pathname === '/login';
+  console.log(isLoginPage);
 
   return (
     <AppShell
@@ -22,24 +25,26 @@ export const DefaultTemplate: FC<DefaultTemplateProps> = ({ children }) => {
       <AppShell.Header>
         <Flex align="center" justify="space-between" p="md" bg={theme.colors.yellow[2]}>
           <Logo />
-          <Flex align="center" justify="flex-end" gap="12px">
-            <Link href="/find" style={{ display: 'flex', alignItems: 'center' }}>
-              <ActionIcon size="lg" color={'white'} variant="subtle" aria-label="Find">
-                <IconUserSearch stroke={2} />
-              </ActionIcon>
-            </Link>
-            <Link href="/mypage" style={{ display: 'flex', alignItems: 'center' }}>
-              <ActionIcon
-                size="lg"
-                style={{ border: '2px solid white' }}
-                radius={'xl'}
-                variant="outline"
-                aria-label="MyPage"
-              >
-                <Avatar src={AVATAR_ICON} size="md" variant="filled" />
-              </ActionIcon>
-            </Link>
-          </Flex>
+          {!isLoginPage && (
+            <Flex align="center" justify="flex-end" gap="12px">
+              <Link href="/find" style={{ display: 'flex', alignItems: 'center' }}>
+                <ActionIcon size="lg" color={'white'} variant="subtle" aria-label="Find">
+                  <IconUserSearch stroke={2} />
+                </ActionIcon>
+              </Link>
+              <Link href="/mypage" style={{ display: 'flex', alignItems: 'center' }}>
+                <ActionIcon
+                  size="lg"
+                  style={{ border: '2px solid white' }}
+                  radius={'xl'}
+                  variant="outline"
+                  aria-label="MyPage"
+                >
+                  <Avatar src={AVATAR_ICON} size="md" variant="filled" />
+                </ActionIcon>
+              </Link>
+            </Flex>
+          )}
         </Flex>
       </AppShell.Header>
       <AppShell.Main>
