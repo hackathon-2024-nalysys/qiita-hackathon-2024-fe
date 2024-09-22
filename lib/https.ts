b71ccc9d9e8153286699d6582de6c223e54bd891
yutaro_matsumoto <spurs03170629@gmail.com>
2024-09-22 09:29:21 +0900

    httpsにwithCredentials設定

--- lib/https.ts
+++ lib/https.ts
@@ -4,4 +4,5 @@ export const https = axios.create({
   baseURL: 'https://hackathon.grainrigi.net/v1',
   timeout: 10000, // タイムアウトを指定（オプション）
   headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
